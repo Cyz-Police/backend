df7a137ba8d7da64424e4d165e9ebef83ddbb04d
Dominykas Seputis <dom.seputis@gmail.com>
2018-03-26 22:22:20 +0300

    Added item creation

--- src/modules/items/controller.js
+++ src/modules/items/controller.js
@@ -1,17 +1,9 @@
 /* eslint prefer-template: "error" */
-/* eslint-env es6 */
+/* eslint func-names: ["error", "never"] */
 import Item from './model';
 import { User } from '../users';
 import { County } from '../countys';
 import { Counter } from '../counters';
-
-export const createItem = async (req, res) => {
-	const { category, author, type, owner, photo, extra } = req.body;
-};
-
-export const a = async (req, res) => {
-
-};
 
 const getUserCountyId = async (id) => {
 	try {
@@ -29,3 +21,28 @@ const getMarkId = async (authorId) => {
 	const lastDidgits = currentYear.slice(-2);
 	return `${coutyId}-${counter}-${lastDidgits}`;
 };
+
+const createItem = async (req, res) => {
+	const {
+		category,
+		author,
+		type,
+		owner,
+		photo,
+		extra,
+	} = req.body;
+	const markId = getMarkId(author);
+
+	const newItem = new Item({
+		category, author, type, owner, photo, extra, markId,
+	});
+
+	try {
+		await newItem.save();
+		return res.status(201).json({ message: 'Item was created', id: markId });
+	} catch (e) {
+		return res.status(404).json({ error: true, message: 'Can not create item' });
+	}
+};
+
+export default createItem;
