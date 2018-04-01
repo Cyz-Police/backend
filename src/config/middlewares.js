b3e4eebbcef0b07df009d1d830e533993319066f
Dominykas Seputis <dom.seputis@gmail.com>
2018-04-01 21:58:37 +0300

    Added passport

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
 import passport from 'passport';
 import morgan from 'morgan';
+import passportConfig from './passport';
 
 export default (app) => {
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(morgan('dev'));
 	app.use(passport.initialize());
+	passportConfig(passport);
 };
---
+++ src/config/passport.js
@@ -0,0 +1,62 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import User from '../modules/users';
+import config from './config';
+
+const JwtOpts = {
+	jwtFromRequest: ExtractJwt.fromAuthHeader(),
+	secretOrKey: config.SECRET,
+};
+
+const jwtUser = new Strategy(JwtOpts, async (payload, done) => {
+	try {
+		const user = await User.findById(payload.sub);
+		if (!user) {
+			return done(null, false);
+		} else if (user.active === false) {
+			return done(null, false);
+		} else if (user.role === '[SUPERADMIN]') {
+			return done(null, false);
+		}
+		return done(null, user);
+	} catch (e) {
+		return done(e, false);
+	}
+});
+
+const jwtAdmin = new Strategy(JwtOpts, async (payload, done) => {
+	try {
+		const user = await User.findById(payload.sub);
+		if (!user) {
+			return done(null, false);
+		} else if (user.active === false) {
+			return done(null, false);
+		} else if (user.role !== '[ADMIN]') {
+			return done(null, false);
+		}
+		return done(null, user);
+	} catch (e) {
+		return done(e, false);
+	}
+});
+
+const jwtSuperadmin = new Strategy(JwtOpts, async (payload, done) => {
+	try {
+		const user = await User.findById(payload.sub);
+		if (!user) {
+			return done(null, false);
+		} else if (user.active === false) {
+			return done(null, false);
+		} else if (user.role !== '[SUPERADMIN]') {
+			return done(null, false);
+		}
+		return done(null, user);
+	} catch (e) {
+		return done(e, false);
+	}
+});
+
+export default (passport) => {
+	passport.use('USER', jwtUser);
+	passport.use('ADMIN', jwtAdmin);
+	passport.use('SUPERADMIN', jwtSuperadmin);
+};
