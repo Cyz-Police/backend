51e11d4b4dd3edc7e26842c567543458fc7eb2a2
Dominykas Seputis <dom.seputis@gmail.com>
2018-05-02 22:48:09 +0300

    Routes fiexes

--- src/modules/items/routes.js
+++ src/modules/items/routes.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import passport from 'passport';
 import * as ItemController from './controller';
 
 const routes = new Router();
 
 routes.post('/item/create', ItemController.createItem);
-routes.get('/item/pull', passport.authenticate('USER', { session: false }), ItemController.makeCsv);
+routes.get('/item/pull', ItemController.makeCsv);
 
 export default routes;
